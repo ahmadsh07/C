1021d95d48c905b1a90eb5eb6be6cacefe084724
Ahmad Shamasnah <amdsh707@gmail.com>
2022-07-13 15:54:39 +0300

    Delete GradesOperations.h

--- GradesOperations.h
+++
@@ -1,7 +0,0 @@
-// 212478994 Ahmad Shamasnah
-#ifndef UNTITLED1_GRADESOPERATIONS_H
-#define UNTITLED1_GRADESOPERATIONS_H
-#include "StudentList.h"
-StudentList *FileToList();
-void Report(StudentList*);
-#endif
